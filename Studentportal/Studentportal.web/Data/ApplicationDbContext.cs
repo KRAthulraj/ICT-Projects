90b650eaba3fc4fd4cdbb8629e07092cdaf3ee54
100790 <100790@MACTECH.LOC>
2025-04-15 19:02:22 +0530

    Initial commit

---
+++ Studentportal/Studentportal.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Studentportal.web.Models.Entities;
+
+namespace Studentportal.web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+           public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
